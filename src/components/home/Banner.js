6d7f41dd85626322003f1cda90d210900de3983a
chinaa477 <karthik.kalvakollu@gmail.com>
2023-02-05 21:16:03 +0530

    Banner Added

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -28,31 +28,31 @@ const sliderSettings = {
     },
   },
   autoplay: {
-    delay: 50000,
+    delay: 5000,
   },
 };
 
 const Banner = () => {
   return (
     <div>
-      <Swiper modules={[Autoplay, EffectCreative, Parallax ]} {...sliderSettings} className="banner-slider">
+      <Swiper modules={[Autoplay, EffectCreative, Parallax]} {...sliderSettings} className="banner-slider">
         {data.map((data, index) => (
           <SwiperSlide key={`banner-${index}`}>
-            <div className="relative z-10 container flex mx-auto">
+            <div className="relative z-10 container mx-auto">
               <div
                 className="w-full h-[600px]"
                 style={{
-                  backgroundImage: `url(${data.image})`,
-                  backgroundPositionX: '58%, 58%',
-                  backgroundPositionY: '11%, 11%',
-                  backgroundSize: 'cover, cover',
-                  backgroundRepeatX: 'no-repeat, no-repeat',
-                  backgroundRepeatY: 'no-repeat, no-repeat',
+                  background: `linear-gradient(to right,RGBA(0,0,0,.2) 0%,RGBA(0,0,0,.5) 100%) 58% 11% / cover no-repeat,url(${data.image}) 58% 11% / cover no-repeat`,
                 }}
               />
-              {/* <div className="title" data-swiper-parallax="-300">
-                Slide 1
-              </div> */}
+              <div className="absolute z-20 top-[20%] left-[2rem] right-[2rem] text-white" data-swiper-parallax="-1000">
+                <div className="text-5xl font-semibold leading-normal" >
+                  {data.title}
+                </div>
+                <div className="text-xl font-normal leading-normal pt-4">
+                  {data.description}
+                </div>
+              </div>
             </div>
             <div
               data-swiper-parallax={"23%"}
