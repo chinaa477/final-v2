693afd05de2b7c439c9dba071a48650b23e83399
chinaa477 <karthik.kalvakollu@gmail.com>
2023-02-16 09:16:32 +0530

    Our Company Data Added

--- src/components/about/CoreValues.js
+++ src/components/about/CoreValues.js
@@ -4,7 +4,7 @@ const CoreValues = () => {
     return (
         <section className="grid items-center mt-12 mx-auto md:px-32 px-5 md:pb-40 pb-10 md:pt-40 pt-8 bg-gradient-2">
             <div className="grid grid-cols gap-4 items-center">
-                <h5 class="text-white text-5xl font-thin">Our Core Values</h5>
+                <h5 className="text-white text-5xl font-thin">Our Core Values</h5>
                 <p className="text-white text-sm pt-16 leading-7">
                     We work in absolute straightforwardness with our customers guaranteeing smooth correspondence between two gatherings in regards to what we do and How we do it. Our submitted hard working attitude will furnish you with an undeterred commitment in making you at last happy with our outcomes.
                 </p>
--- src/components/about/OurCompany.js
+++ src/components/about/OurCompany.js
@@ -1,46 +1,42 @@
 import React from "react"
 import divider from '../../images/divider.png'
-import { Autoplay, EffectFade } from 'swiper';
+import { Autoplay, EffectFade, Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-
-const sliderSettings = {
-    slidesPerView: '1',
-    loop: true,
-    effect: "fade",
-    autoplay: {
-        delay: 5000,
-    },
-};
+import data from '../../../content/about-us/our-company.json'
 
 const OurCompany = () => {
     return (
         <section className="grid grid-cols-1 items-center w-full mx-auto pt-16 pb-24 bg-light">
             <div className="relative flex items-center justify-center overflow-hidden pb-24">
                 <div className="container mx-auto grid items-center justify-center text-center gap-8">
-                    <h2 className="text-primary text-[2.75rem]">Our Company</h2>
-                    <h3 className="text-dark-muted tracking-[.5rem] text-xl font-medium">SERVING THE BEST</h3>
+                    <h2 className="text-primary text-[2.75rem]">{data.heading}</h2>
+                    <h3 className="text-dark-muted tracking-[.5rem] text-xl font-medium">{data.subHeading}</h3>
                     <img className="mx-auto" src={divider} />
                     <p className="text-dark-muted font-semibold text-base leading-7 px-52">
-                        Fantastic food merits incredible regard. We carry that marvel to each dish we make.We centre around newness and flavor first. We do not utilize counterfeit hues and fake flavors.
+                        {data.description}
                     </p>
                 </div>
             </div>
             <div className="container mx-auto -mb-80">
                 <div className="px-72">
-                    <Swiper modules={[Autoplay, EffectFade]} {...sliderSettings} className="shadow-2xl">
-                        <SwiperSlide>
-                            <img src="https://housefullevents.virajtechnologies.com/wp-content/uploads/2019/12/hfe31.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://housefullevents.virajtechnologies.com/wp-content/uploads/2019/12/hfe36.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://housefullevents.virajtechnologies.com/wp-content/uploads/2019/12/hfe35.jpg" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://housefullevents.virajtechnologies.com/wp-content/uploads/2019/12/hfe33.jpg" />
-                        </SwiperSlide>
+                    <Swiper loop={true} navigation={{
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev",
+                    }}
+                        modules={[Navigation, Autoplay, EffectFade]}
+                        className="shadow-2xl">
+                        <div className="absolute inset-y-0 left-2 z-10 flex items-center">
+                            <button className="swiper-button-prev -ml-2 lg:-ml-4 flex justify-center items-center shadow focus:outline-none after:text-white"></button>
+                        </div>
+                        <div className="absolute inset-y-0 right-2 z-10 flex items-center">
+                            <button className="swiper-button-next -mr-2 lg:-mr-4 flex justify-center items-center shadow focus:outline-none after:text-white"></button>
+                        </div>
+                        {data.gallery.map((gallery, index) => (
+                            <SwiperSlide key={`gallery-${index}`}>
+                                <img src={gallery.image} />
+                            </SwiperSlide>
+                        ))}
                     </Swiper>
                 </div>
             </div>
